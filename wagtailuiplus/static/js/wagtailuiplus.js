c587b80bc4809f5c15166bd94bbacd5056b22259
David Conde <david@luggagecare.com>
2019-09-25 13:59:41 +0200

    Improved usage of width for stream fields. Cleaned up javascript code.

--- wagtailuiplus/static/js/wagtailuiplus.js
+++ wagtailuiplus/static/js/wagtailuiplus.js
@@ -1,5 +1,5 @@
 // Event handler for choice blocks with conditional visibility
-function choiceHandler(target) {
+function onChoiceHandlerChange(target) {
   const choiceHandler = target.closest('.wagtailuiplus__choice-handler');
   if (choiceHandler !== null) {
     const choiceHandlerValue = choiceHandler.querySelector('select').value;
@@ -56,24 +56,8 @@ function choiceHandler(target) {
   }
 }
 
-// Make the title bar of a struct block show the title of the first char block
-function updateStructBlockHeader(event) {
-  const field = event.target.closest('li');
-  if (event.target.tagName !== 'INPUT' || field === null || field.previousElementSibling !== null) {
-    return;
-  }
-  const headerLabel = field.closest('.sequence-member').querySelector('.sequence-controls > h3 > label');
-  if (headerLabel === null) {
-    return;
-  }
-  if (!headerLabel.hasAttribute('data-original-text')) {
-    headerLabel.dataset.originalText = headerLabel.innerText;
-  }
-  headerLabel.innerText = headerLabel.dataset.originalText + ' - ' + event.target.value;
-}
-
 // Event handler for checkboxes with interactivity
-function checkboxHandler(checkboxHandler) {
+function onCheckboxHandlerChange(checkboxHandler) {
   const isChecked = checkboxHandler.checked;
   searchContainer = checkboxHandler.closest('section');
   const checkboxHandlerIdRegex = /wagtailuiplus__checkbox-handler--([a-zA-Z\-\_]+)/;
@@ -89,7 +73,7 @@ function checkboxHandler(checkboxHandler) {
 }
 
 // Event handler for checkboxes with interactivity - Reverse dependency
-function checkboxHandlerTarget(checkboxHandlerTarget) {
+function onCheckboxHandlerTargetChange(checkboxHandlerTarget) {
   const isChecked = checkboxHandlerTarget.checked;
   if (isChecked) {
     return;
@@ -101,130 +85,176 @@ function checkboxHandlerTarget(checkboxHandlerTarget) {
   checkboxHandler.checked = false;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
 
-  // Make the panels collapsable
-  let i;
-  const panelHeaders = document.querySelectorAll('.object > .title-wrapper');
-  for (i = 0; i < panelHeaders.length; i++) {
-    panelHeaders[i].addEventListener('click', function() {
-      if (this.parentElement.classList.contains('wagtailuiplus__panel--collapsed')) {
-        this.parentElement.classList.remove('wagtailuiplus__panel--collapsed');
-      } else {
-        this.parentElement.classList.add('wagtailuiplus__panel--collapsed');
-      }
-    });
-  }
+// Initialize a collapsible panel
+function initCollapsablePanel(panelHeader) {
+  panelHeader.addEventListener('click', function() {
+    if (this.parentElement.classList.contains('wagtailuiplus__panel--collapsed')) {
+      this.parentElement.classList.remove('wagtailuiplus__panel--collapsed');
+    } else {
+      this.parentElement.classList.add('wagtailuiplus__panel--collapsed');
+    }
+  });
+}
 
-  let sequenceControls;
-  const structBlockContainers = document.querySelectorAll('.sequence-container.sequence-type-stream > .sequence-container-inner > .sequence');
-  for (i = 0; i < structBlockContainers.length; i++) {
-    // Make the struct block headers collapsable
-    structBlockContainers[i].addEventListener('click', function(event) {
-      sequenceControls = event.target.closest('.sequence-controls');
-      if (sequenceControls === null) {
-        return;
-      }
-      if (this.id !== event.target.closest('.sequence').id) {
-        return;
-      }
+// Initialize a collapsable struct block
+function initCollapsableStructBlock(structBlockContainer) {
+  structBlockContainer.addEventListener('click', function(event) {
+    const sequenceControls = event.target.closest('.sequence-controls');
+    if (sequenceControls === null) {
+      return;
+    }
+    if (this.id !== event.target.closest('.sequence').id) {
+      return;
+    }
 
-      if (sequenceControls.parentElement.classList.contains('wagtailuiplus__struct-block--collapsed')) {
-        sequenceControls.parentElement.classList.remove('wagtailuiplus__struct-block--collapsed');
-      } else {
-        sequenceControls.parentElement.classList.add('wagtailuiplus__struct-block--collapsed');
-      }
-    });
+    if (sequenceControls.parentElement.classList.contains('wagtailuiplus__struct-block--collapsed')) {
+      sequenceControls.parentElement.classList.remove('wagtailuiplus__struct-block--collapsed');
+    } else {
+      sequenceControls.parentElement.classList.add('wagtailuiplus__struct-block--collapsed');
+    }
+  });
+}
 
-    // Make the first field of a struct block update the header text
-    structBlockContainers[i].addEventListener('change', function(event) {
-      updateStructBlockHeader(event);
-    });
-    structBlockContainers[i].addEventListener('keyup', function(event) {
-      updateStructBlockHeader(event);
-    });
-  }
+// Initialize a smart struct block header
+function initSmartStructBlockHeader(structBlockContainer) {
+  structBlockContainer.addEventListener('change', function(event) {
+    onSmartStructBlockHeaderChange(event);
+  });
+  structBlockContainer.addEventListener('keyup', function(event) {
+    onSmartStructBlockHeaderChange(event);
+  });
 
-  // Set the initial collapsed state of existing struct blocks
   let fields;
   let headerLabel;
-  const structBlocks = document.querySelectorAll('.sequence-container.sequence-type-stream > .sequence-container-inner > .sequence > .sequence-member');
+  const structBlocks = structBlockContainer.children;
   for (i = 0; i < structBlocks.length; i++) {
-    // structBlocks[i].classList.add('wagtailuiplus__struct-block--collapsed');
+    if (!structBlocks[i].classList.contains('sequence-member')) {
+      continue;
+    }
     fields = structBlocks[i].querySelectorAll('.field');
     if (fields.length === 0) {
       continue;
     }
     headerLabel = structBlocks[i].querySelector('.sequence-controls > h3 > label');
     headerLabel.dataset.originalText = headerLabel.innerText;
+    // If the first block in the struct block is a char block
     if (fields[0].classList.contains('char_field')) {
-      structBlocks[i].classList.add('wagtailuiplus__struct-block--collapsed'); // remove this when uncommenting the line above
+      // Set the initial collapsed state and set the header smart title based on the char block value
+      structBlocks[i].classList.add('wagtailuiplus__struct-block--collapsed');
       headerLabel.innerText = headerLabel.dataset.originalText + ' - ' + fields[0].querySelector('input[type=text]').value;
-    } else if (fields[0].classList.contains('model_choice_field')) {
-      // uncomment this to enable model choice field based headers, todo: update header on change of model choice
-      // headerLabel.innerText = headerLabel.dataset.originalText + ' - ' + fields[0].querySelector('.chosen .title').innerText;
     }
   }
+}
 
-  // Bind the choice block event handler to all stream fields, so it applies to all existing and future choice blocks
-  for (i = 0; i < structBlockContainers.length; i++) {
-    structBlockContainers[i].addEventListener('change', function(event) {
-      choiceHandler(event.target);
-    });
+// Event handler for a smart struct block header change
+function onSmartStructBlockHeaderChange(event) {
+  const field = event.target.closest('li');
+  if (event.target.tagName !== 'INPUT' || field === null || field.previousElementSibling !== null) {
+    return;
   }
-
-  // Trigger the choice block event handler for all existing choice blocks
-  const choiceHandlerSelects = document.querySelectorAll('.sequence-container.sequence-type-stream > .sequence-container-inner > .sequence .wagtailuiplus__choice-handler select');
-  for (i = 0; i < choiceHandlerSelects.length; i++) {
-    choiceHandler(choiceHandlerSelects[i]);
+  const headerLabel = field.closest('.sequence-member').querySelector('.sequence-controls > h3 > label');
+  if (headerLabel === null) {
+    return;
   }
+  if (!headerLabel.hasAttribute('data-original-text')) {
+    headerLabel.dataset.originalText = headerLabel.innerText;
+  }
+  headerLabel.innerText = headerLabel.dataset.originalText + ' - ' + event.target.value;
+}
 
-  // Watch for new blocks being added to stream fields
-  const config = { attributes: false, childList: true, subtree: false };
-  const callback = function(mutations, observer) {
-      let k;
-      let l;
-      let choiceHandlerSelects;
-      for (let j = 0; j < mutations.length; j++) {
-        for (k = 0; k < mutations[j].addedNodes.length; k++) {
-          // Make sure the choice handler is run for each new choice block
-          choiceHandlerSelects = mutations[j].addedNodes[k].querySelectorAll('.wagtailuiplus__choice-handler select');
-          for (l = 0; l < choiceHandlerSelects.length; l++) {
-            choiceHandler(choiceHandlerSelects[l]);
-          }
+
+// Initialize a choice handler
+function initChoiceHandler(structBlockContainer) {
+  structBlockContainer.addEventListener('change', function(event) {
+    onChoiceHandlerChange(event.target);
+  });
+}
+
+// Initialize the struct block watcher
+function initStructBlockWatcher(structBlockContainer) {
+  const observer = new MutationObserver(onNewStructBlock);
+  observer.observe(structBlockContainer, {
+    attributes: false,
+    childList: true,
+    subtree: false,
+  });
+}
+
+// Event handler for when a new struct block is created
+function onNewStructBlock(mutations, observer) {
+    let k;
+    let l;
+    let choiceHandlerSelects;
+    for (let j = 0; j < mutations.length; j++) {
+      for (k = 0; k < mutations[j].addedNodes.length; k++) {
+        // Make sure the choice handler is run for each new choice block
+        choiceHandlerSelects = mutations[j].addedNodes[k].querySelectorAll('.wagtailuiplus__choice-handler select');
+        for (l = 0; l < choiceHandlerSelects.length; l++) {
+          onChoiceHandlerChange(choiceHandlerSelects[l]);
         }
       }
-  };
-  let observer;
-  for (i = 0; i < structBlockContainers.length; i++) {
-    observer = new MutationObserver(callback);
-    observer.observe(structBlockContainers[i], config);
+    }
+}
+
+// Initialize a checkbox handler
+function initCheckboxHandler(checkboxHandlerInput) {
+  checkboxHandlerInput.addEventListener('change', function(event) {
+    onCheckboxHandlerChange(event.target);
+  });
+}
+
+// Initialize a checkbox handler target
+function initCheckboxHandlerTarget(checkboxHandlerTargetInput) {
+  checkboxHandlerTargetInput.addEventListener('change', function(event) {
+    onCheckboxHandlerTargetChange(event.target);
+  });
+}
+
+// Initialize all event handlers on page load
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Initialize collapsable panels
+  let i;
+  const panelHeaders = document.querySelectorAll('.object > .title-wrapper');
+  for (i = 0; i < panelHeaders.length; i++) {
+    initCollapsablePanel(panelHeaders[i]);
   }
 
-  // Bind the char field choice event handlers, and trigger it once
+  // Initialize stream field related features
+  let observer;
+  const structBlockContainers = document.querySelectorAll('.sequence-container.sequence-type-stream > .sequence-container-inner > .sequence');
+  for (i = 0; i < structBlockContainers.length; i++) {
+    initCollapsableStructBlock(structBlockContainers[i]);
+    initSmartStructBlockHeader(structBlockContainers[i]);
+    initChoiceHandler(structBlockContainers[i]);
+    initStructBlockWatcher(structBlockContainers[i]);
+  }
+
+  // Set the initial state of choice handlers contained in stream fields
+  const choiceHandlerSelects = document.querySelectorAll('.sequence-container.sequence-type-stream > .sequence-container-inner > .sequence .wagtailuiplus__choice-handler select');
+  for (i = 0; i < choiceHandlerSelects.length; i++) {
+    onChoiceHandlerChange(choiceHandlerSelects[i]);
+  }
+
+  // Initialize choice handlers not contained in stream fields
   const choiceHandlersCharFieldSelects = document.querySelectorAll('li.wagtailuiplus__choice-handler select');
   for (i = 0; i < choiceHandlersCharFieldSelects.length; i++) {
-    choiceHandlersCharFieldSelects[i].addEventListener('change', function(event) {
-      choiceHandler(event.target);
-    });
-    choiceHandler(choiceHandlersCharFieldSelects[i]);
+    initChoiceHandler(choiceHandlersCharFieldSelects[i]);
+    onChoiceHandlerChange(choiceHandlersCharFieldSelects[i]);
   }
 
-  // Bind the checkbox handler for interactive checkboxes
+  // Initialize checkbox handlers
   const checkboxHandlerInputs = document.querySelectorAll('li.wagtailuiplus__checkbox-handler input[type=checkbox]');
   for (i = 0; i < checkboxHandlerInputs.length; i++) {
-    checkboxHandlerInputs[i].addEventListener('change', function(event) {
-      checkboxHandler(event.target);
-    });
-    checkboxHandler(checkboxHandlerInputs[i]);
+    initCheckboxHandler(checkboxHandlerInputs[i]);
+    onCheckboxHandlerChange(checkboxHandlerInputs[i]);
   }
 
-  // Bind the checkbox handler targets for interactive checkboxes - reverse dependency
+  // Initialize checkbox handler targets
   const checkboxHandlerTargetInputs = document.querySelectorAll('li[class^="wagtailuiplus__checkbox-handler-target--"] input[type=checkbox], li[class*=" wagtailuiplus__checkbox-handler-target--"] input[type=checkbox]');
   for (i = 0; i < checkboxHandlerTargetInputs.length; i++) {
-    checkboxHandlerTargetInputs[i].addEventListener('change', function(event) {
-      checkboxHandlerTarget(event.target);
-    });
-    checkboxHandlerTarget(checkboxHandlerTargetInputs[i]);
+    initCheckboxHandlerTarget(checkboxHandlerTargetInputs[i]);
+    onCheckboxHandlerTargetChange(checkboxHandlerTargetInputs[i]);
   }
 });
